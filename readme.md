661a0de9c7b6cb37d8c2a49edc47abcbde14ade5
Fanny1203 <boitard@gmail.com>
2024-12-28 00:21:39 +0100

    responsive design + qq changements

---
+++ readme.md
@@ -0,0 +1,57 @@
+<a name="readme-top"></a>
+<!--
+*** Thanks for using Document My Project. (https://github.com/luisvent/document_my_project) 
+*** If you have a suggestion that would make this better, please fork  
+*** the repo and create a pull request or simply open an issue.
+*** Don't forget to give the project a star!
+-->
+
+<p align="center"><a href="https://github.com///graphs/contributors"><img src="https://img.shields.io/github/contributors//.svg?style=for-the-badge" alt="Contributors"></a>
+        <a href="https://github.com///network/members"><img src="https://img.shields.io/github/forks//.svg?style=for-the-badge" alt="Forks"></a>
+        <a href="https://github.com///stargazers"><img src="https://img.shields.io/github/stars//.svg?style=for-the-badge" alt="Stargazers"></a>
+        <a href="https://github.com///issues"><img src="https://img.shields.io/github/issues//.svg?style=for-the-badge" alt="Issues"></a></p><br/>
+
+
+<div align="center">
+
+
+
+# Expérience colgate
+
+Ne pas lire cette description avant de faire l'expérience!
+Il s'agit de se rendre compte de notre "cécité attentionnelle".
+On est tellement focalisé sur les taches de salade sur les dents des personnages qu'on manque des informations pourtant évidentes.
+
+</div>
+
+
+<!-- LINKS_PLACEHOLDER -->
+
+## ️Table of Contents
+ <details>
+<summary>Open Contents</summary>
+
+- [Expérience colgate](#exprience-colgate)
+  - [About the Project](#about-the-project)
+  - [About the Author](#about-the-author)
+</details>
+
+## About the Project
+
+https://divers.fannyboitard.fr/cecite_colgate/
+
+
+
+
+## About the Author
+
+**Fanny Boitard**
+
+This project was created by Fanny Boitard. Connect with me on [GitHub](https://github.com/Fanny1203)  to learn more about my projects and professional background.
+
+
+<p align="right"><a href="#readme-top">(Back to top)</a></p>
+
+---
+ <div align="center">Built with ❤️ with <a href="https://github.com/luisvent/document_my_project">Document My Project</a></div>
+
--- sketch.js
+++ sketch.js
@@ -16,13 +16,13 @@ let etape = 0;
 
 // Tableau des rectangles pour chaque image
 // Format: [x, y, largeur, hauteur] relatif à l'image
-const coordonnees_rectangles = [[0.75, 0.6, 0.4, 0.4], [0.65, 0.35, 0.35, 0.35], [0.37, 0.3, 0.1, 0.3]];
+const coordonnees_rectangles = [[0.75, 0.5, 0.24, 0.4], [0.65, 0.3, 0.35, 0.35], [0.37, 0.25, 0.1, 0.3]];
 
 // Tableau des légendes pour chaque image
 const labels = [
     "Cette main a vraiment trop de doigts !",
     "A qui appartient cette main ?",
-    "Hu, ce monsieur n'a pas d'oreille droite !"
+    "Hum, ce monsieur n'a pas d'oreille droite !"
 ];
 
 const intermediateText1 = "Vous avez vu les bouts de salade dans les dents des gens ? Mais avez-vous vu le reste ?!";
@@ -31,7 +31,7 @@ const intermediateText2 = "Vérifions ensemble !";
 
 const conclusionText = `La cécité attentionnelle est un phénomène psychologique où notre cerveau peut complètement manquer des éléments pourtant visibles quand notre attention est focalisée sur une tâche spécifique.
 Le rectangle rouge que vous avez vu dans le troisième tour était présent pour vous permettre de mettre en évidence une zone importante que vous avez peut-être manquée lors des premiers visionnages.
-Il s'agit d'une campagne publicitaire pour Colgate: quel est le message, à votre avis ?
+\nIl s'agit d'une campagne publicitaire pour Colgate: quel est le message, à votre avis ?
 Source: https://www.publicidadeuskadi.com/ingeniosa-campana-de-publicidad-de-colgate`;
 
 function preload() {
@@ -42,113 +42,112 @@ function preload() {
 }
 
 function setup() {
-    canvas = createCanvas(800, 600);
+    // Calcul de la taille du canvas en fonction de la fenêtre
+    let canvasWidth = min(windowWidth * 0.8, 800);
+    let canvasHeight = (canvasWidth * 3) / 4;
+    if(canvasHeight > windowHeight) {
+        canvasHeight = windowHeight*0.8;
+        console.log("canvasHeight", canvasHeight);
+        console.log("windowHeight", windowHeight);
+        canvasWidth = (canvasHeight * 4) / 3;
+    }
+    
+    
+    // Création du canvas
+    canvas = createCanvas(canvasWidth, canvasHeight);
     canvas.parent('canvas-container');
     
     // Create message container
     messageContainer = createDiv('');
     messageContainer.parent('canvas-container');
     messageContainer.class('message-container');
-    messageContainer.hide();
     
-    // Create the Next button and add it to the canvas container
+    // Create and style the next button
     nextButton = createButton('Suivant');
     nextButton.parent('canvas-container');
     nextButton.mousePressed(nextImage);
-    
-    // Create the restart button
-    restartButton = createButton('Recommencer l\'expérience');
+    nextButton.class('control-button');
+
+
+    // Create and style the restart button
+    restartButton = createButton('Recommencer');
     restartButton.parent('canvas-container');
     restartButton.mousePressed(restartExperiment);
-    restartButton.hide();
-    
+    restartButton.class('control-button');
 
+    restartExperiment();
 }
 
 function draw() {
-    if (etape === 1 || etape === 3 || etape === 5) {
-        // Cacher le canvas pour les étapes de texte
-        canvas.hide();
-        messageContainer.show();
-        
-        // Afficher le message approprié
-        let message = '';
-        if (etape === 1) message = intermediateText1;
-        else if (etape === 3) message = intermediateText2;
-        else if (etape === 5) {
-            // Formater le texte de conclusion avec des paragraphes HTML
-            message = conclusionText.split('\n\n').map(p => `<p class="message-text">${p}</p>`).join('');
-            // Convertir le dernier lien en vrai lien HTML
-            message = message.replace(/(https:\/\/[^\s]+)/, '<a href="$1" class="source-link" target="_blank">$1</a>');
-        }
-        messageContainer.html(message);
+    if (etape === 1) {
+        displayMessage(intermediateText1);
+    } else if (etape === 3) {
+        displayMessage(intermediateText2);
+    } else if (etape === 5) {
+        displayMessage(conclusionText);
+        nextButton.hide();restartButton.show();
     } else {
-        // Montrer le canvas et cacher le conteneur de message pour les étapes d'images
+        // Afficher le canvas et masquer le message pour les étapes d'images
         canvas.show();
         messageContainer.hide();
         
-        background(220);
+        background(255);
+        
         if (images[currentImageIndex]) {
-            let img = images[currentImageIndex];
-            let imgX = (width - img.width) / 2;
-            let imgY = (height - img.height) / 2;
+            // Calculer les dimensions pour l'image
+            let imgAspect = images[currentImageIndex].width / images[currentImageIndex].height;
+            let imgWidth = width * 0.9;
+            let imgHeight = imgWidth / imgAspect;
             
-            image(img, imgX, imgY);
+            // Centrer l'image
+            let imgX = (width - imgWidth) / 2;
+            let imgY = (height - imgHeight) / 2;
             
+            // Dessiner l'image
+            image(images[currentImageIndex], imgX, imgY, imgWidth, imgHeight);
+            
+            // Si nous sommes à l'étape 4, dessiner le rectangle et le texte
             if (etape === 4) {
-                noFill();
+                let rectCoords = coordonnees_rectangles[currentImageIndex];
                 stroke(255, 0, 0);
-                strokeWeight(3);
-                const [rectX, rectY, rectWidth, rectHeight] = coordonnees_rectangles[currentImageIndex];
-                rect(rectX*img.width, rectY*img.height, rectWidth*img.width, rectHeight*img.height);
+                strokeWeight(2);
+                noFill();
                 
-                textAlign(CENTER);
-                textSize(18);
-                fill(0);
+                // Dessiner le rectangle avec les coordonnées ajustées
+                let rectX = imgX + (rectCoords[0] * imgWidth);
+                let rectY = imgY + (rectCoords[1] * imgHeight);
+                let rectW = rectCoords[2] * imgWidth;
+                let rectH = rectCoords[3] * imgHeight;
+                rect(rectX, rectY, rectW, rectH);
+                
+                // Afficher le texte sous l'image
                 noStroke();
-                text(labels[currentImageIndex], width/2, imgY + img.height + 30);
+                fill(0);
+                textAlign(CENTER, TOP);
+                textSize(min(width * 0.03, 16));
+                text(labels[currentImageIndex], width/2, imgY + imgHeight + 10);
             }
         }
     }
 }
 
-function splitTextIntoLines(text, maxWidth) {
-    let words = text.split(' ');
-    let lines = [];
-    let currentLine = words[0];
 
-    for (let i = 1; i < words.length; i++) {
-        let testLine = currentLine + ' ' + words[i];
-        if (textWidth(testLine) < maxWidth) {
-            currentLine = testLine;
-        } else {
-            lines.push(currentLine);
-            currentLine = words[i];
-        }
+function keyPressed() {
+    // Code 39 correspond à la flèche droite
+    if (keyCode === 39) {
+        nextImage();
     }
-    lines.push(currentLine);
-    return lines;
 }
 
 function nextImage() {
-    // Si on est sur un message intermédiaire, passer directement à l'étape suivante
-    if (etape === 1 || etape === 3) {
-        etape++;
-        return;
-    }
-    
-    currentImageIndex++;
-    
-    // Si on a fini de montrer toutes les images du passage actuel
-    if (currentImageIndex >= images.length) {
-        currentImageIndex = 0;
-        etape++;
-        
-        // Si on arrive à la conclusion
-        if (etape === 5) {
-            nextButton.hide();
-            restartButton.show();
+    if (etape === 0 || etape === 2 || etape === 4) {
+        currentImageIndex++;
+        if (currentImageIndex >= images.length) {
+            currentImageIndex = 0;
+            etape++;
         }
+    } else {
+        etape++;
     }
 }
 
@@ -160,3 +159,17 @@ function restartExperiment() {
     canvas.show();
     messageContainer.hide();
 }
+
+
+function displayMessage(message) {
+    // Masquer le canvas et afficher le conteneur de message
+    canvas.hide();
+    messageContainer.show();
+    
+    message = message.split('\n\n').map(p => `<p class="message-text">${p}</p>`).join('');
+    message = message.replace(/(https:\/\/[^\s]+)/, '<a href="$1" class="source-link" target="_blank">$1</a>');
+    
+    messageContainer.html(message);
+}
+
+
